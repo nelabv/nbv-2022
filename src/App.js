b54ff5b9038c4c60394238f042a64f657cef3efc
nelabv <nielleangelabv@gmail.com>
2022-05-12 09:41:38 +0800

    Information added

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1>Nielle 2022</h1>
-      <span>Portfolio</span>
+      <h1>Nielle &#127800;</h1>
+      <span>Frontend Developer | UI/UX Designer</span>
     </div>
   );
 }
