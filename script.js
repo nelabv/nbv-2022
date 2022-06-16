3656d4ee2588c066c643eaa2f623a7eda2435ad0
nelabv <nielleangelabv@gmail.com>
2022-06-16 10:11:16 +0800

    Nav opacity change on scroll

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('scroll', function(e) {
+  const nav = document.getElementById("nav");
+
+  if (window.scrollY > 300) {
+      nav.style.backgroundColor = "rgb(248, 242, 220)";
+  } else {
+      nav.style.backgroundColor = "#FFFDF6";
+  }   
+  });
